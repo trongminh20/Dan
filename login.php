59862a4bbb83cbb7cf79e715c553a93f168c1bca
dlock15 <58092170+dlock15@users.noreply.github.com>
2019-12-04 12:30:23 -0800

    Add files via upload

--- login.php
+++ login.php
@@ -2,62 +2,53 @@
 $pageTitle = "Famous Quotes";
 $page = "Registration";
 include ("./template/header.html");
-
-
 session_start();
 if ((!filter_input(INPUT_POST, 'username')) || (!filter_input(INPUT_POST, 'password'))) {
     header("Location: userlogin.php");
     exit;
 }
-
 //connect to server and select database
-
 if (isset($_POST['submit'])) {
-    $sqlConnect = mysqli_connect('localhost', 'root', 'trongminh', 'quoteDB');
-
+    $sqlConnect = mysqli_connect('localhost', 'root', 'letmein', 'QouteDb');
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
     }
-
-
 //$userName = filter_input(INPUT_POST, 'username');
     $name = $_POST['username'];
     $userpasswd = $_POST['password'];
-
-    $query = "select username from users where password = PASSWORD('$userpasswd') and username ='$name';";
-
+    
+    //"SELECT f_name, l_name FROM auth_users WHERE username = '".$targetname.
+      //  "' AND password = PASSWORD('".$targetpasswd."')";
+    
+    $query = "select username, email from Users where username = '".$name."' and password = '".$userpasswd."'";
     $result = mysqli_query($sqlConnect, $query) or die(mysqli_error($sqlConnect));
-
-    $_SESSION['username'] = $name;
+    //$_SESSION['username'] = $name;
 //get the number of rows in the result set; should be 1 if a match
-
     if (mysqli_num_rows($result) == 1) {
-
         //if authorized, get the values of f_name l_name
         while ($info = mysqli_fetch_array($result)) {
             $name = stripslashes($info['username']);
+            $email = stripslashes($info['email']);
         }
-
-
+     
         //set authorization cookie using curent Session ID
         setcookie("auth", session_id(), time() + 60 * 30, "/", "", 0);
-
         //create display string
         ?>
-        <h2>list quote</h2>
+        <h2>list of quotes user has added</h2>
         <?php
-        $queryQuote = "SELECT * 
-                        FROM USERS u, quotes q
-                        WHERE u.email = q.email;
-                        ";
-        //$result2 = mysqli_query($sqlConnect, $query) or die(mysqli_error($sqlConnect));
+       // echo "$email";
+        $queryQuote = "SELECT email FROM quotes";
+                       
+                       //WHERE email = '".$email."'";
 
+                        
+        //$result2 = mysqli_query($sqlConnect, $query) or die(mysqli_error($sqlConnect));
         if ($result2 = $sqlConnect->query($queryQuote)) {
             while ($row = $result2->fetch_assoc()) {
                 $quote = stripslashes($row['quote']);
                 $auth = stripslashes($row['author']);
-
                 echo
                 "<div class=\"quote-list\">"
                 . "<tr style=\"font-size:15px;border-bottom: 1px solid black;\">"
@@ -66,9 +57,11 @@ if (isset($_POST['submit'])) {
                 . "</tr> "
                 . "</div>";
             }
-
             $result->free();
             //setcookie("auth", session_id(), time() + 60 * 30, "/", "", 0);
+        }
+        else{
+            echo "not working properly";
         }
         ?>
         <h2>adding quote</h2>
@@ -83,14 +76,16 @@ if (isset($_POST['submit'])) {
             <input type="text" name="author" value="">
             <br><br>
             <input type="submit" value="submit" name="submit">
-        </form> 
+        </form> "
         <form action="./logout.php" method="post">
             <input type="submit" name="logout" value="logout">
 
-        </form>
+        </form
 
         <?php
     } else {
+        echo "Invalid Login";
+        
         echo mysqli_error($sqlConnect);
     }
 }
